f05b79b23e8e78f216c7d64b4881e8c6dee7f634
10780 <discocircus001@gmail.com>
2020-06-30 12:59:09 -0700

    Add files via upload

---
+++ andgate.vhd
@@ -0,0 +1,19 @@
+--AND gate
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity and_gate is
+	port(
+	--inputs & outputs
+		in1 : in std_logic;
+		in2 : in std_logic;
+		result : out std_logic;
+	);
+end and_gate;
+
+architecture rtl of and_gate is
+	signal example_and : std_logic;
+begin
+	example_and <= in1 and in2; --operation for the 2 inputs
+	result <= example_and; --assign result variable with whatever the result of AND operation is 
+end rtl;
